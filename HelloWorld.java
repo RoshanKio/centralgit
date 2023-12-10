d15b93ae2003bba369eff1957ff4c6c61197d3de
RoshanKio <131989902+RoshanKio@users.noreply.github.com>
2023-12-09 22:52:38 -0500

    3rd Commit form Singapore

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args) {
+		System.out.println("Welcome to my First Java Code");
 		for(int i = 1; i<=10; i++) {
 			if(i==3) {
 				continue;
